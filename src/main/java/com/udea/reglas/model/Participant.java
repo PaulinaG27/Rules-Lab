e132464448cbb97aaa5075d9c01d0a3c51d84b2d
PaulinaG27 <paulina.garcia1@udea.edu.co>
2024-10-06 17:34:29 -0500

    Primeras 4 reglas agregadas junto a las ya existentes

---
+++ src/main/java/com/udea/reglas/model/Participant.java
@@ -0,0 +1,23 @@
+package com.udea.reglas.model;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Data
+public class Participant {
+
+    private String name;
+    private int age;
+    private int creditScore;
+    private long annualSalary;
+    private long existingDebt;
+    private long loanAmount;
+    private boolean haveJob;
+    private boolean haveJudicialBackground;
+    private int quantityDependants;
+
+
+}
